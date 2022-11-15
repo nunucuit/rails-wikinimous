0e610a8994a77a7797db8e9c14b49e49c15614c8
nunucuit <rises-426@hotmail.fr>
2022-11-15 17:05:01 +0100

    merci teddy

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,12 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+10.times do
+  article = Article.new(
+    title: Faker::Fantasy::Tolkien.character,
+    content: Faker::Fantasy::Tolkien.poem
+  )
+  article.save!
+end
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
